c87af748583ec44b44518e19758c946529ff62be
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2022-06-11 14:40:36 +0900

    Create queue.py

---
+++ basic/dfs+bfs/queue.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+queue = deque()
+
+queue.append(5)
+queue.append(4)
+queue.append(1)
+queue.popleft()
+
+print(queue)
+queue.reverse() # 역순으로 바꾸기
+print(queue)
+
+print(list(queue)) # deque 객체를 리스트 자료형으로 
