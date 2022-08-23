3a0c83a42aa32126c7877496ad35d694e2c7ad0e
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2022-08-23 15:33:45 +0900

    Create bs2.py

---
+++ basic/binary_search/bs2.py
@@ -0,0 +1,22 @@
+''''
+반복문을 사용한 이진 탐색
+''''
+
+import sys 
+r = sys.stdin.readline
+
+n, target = map(int, r().split(" "))
+lst = list(map(int, r().split(" ")))
+start = 0
+end = n-1
+
+while True:
+    if start>end:
+        print("해당 값이 없습니다")
+    mid = (start+end)//2
+    if lst[mid] == target:
+        print(mid+1)
+    elif lst[mid]>target:
+        end = mid-1
+    else:
+        start = mid+1
