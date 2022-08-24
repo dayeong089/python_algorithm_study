97310bf4b2e7371ce19e543c8caf8fc128af2196
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2022-08-24 22:13:25 +0900

    Create to1.py

---
+++ basic/dp/to1.py
@@ -0,0 +1,16 @@
+import sys
+r = sys.stdin.readline
+
+lst = [0] * 30001
+
+x = int(r())
+for i in range(2, x+1):
+    lst[i] = lst[i-1] + 1
+    if i%2==0:
+        lst[i] = min(lst[i//2]+1, lst[i])
+    if i%3==0:
+        lst[i] = min(lst[i//3]+1, lst[i])
+    if i%5==0:
+        lst[i] = min(lst[i//5]+1, lst[i])
+
+print(lst[x])
