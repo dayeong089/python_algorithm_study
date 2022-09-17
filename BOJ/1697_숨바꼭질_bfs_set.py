9371737224cfc1ba4573cf238e7692bfa94ad8fd
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2022-09-17 21:52:50 +0900

    Update 1697_숨바꼭질_bfs_set.py

--- BOJ/1697_숨바꼭질_bfs_set.py
+++ BOJ/1697_숨바꼭질_bfs_set.py
@@ -9,14 +9,12 @@ def bfs(n, k):
     if n == k:
         print(0)
         return
-
     if n > k:
         print(n-k)
         return
 
     q = deque()
     q.append((n, 0))
-
     while q:
         loc, time = q.popleft()
         now = time+1
@@ -31,6 +29,7 @@ def bfs(n, k):
                 return
             else:
                 q.append((loc1, now))
+                
         if 0<=loc2<=100000 and loc2 not in lst:
             lst.add(loc2)
             if loc2==k:
@@ -38,6 +37,7 @@ def bfs(n, k):
                 return
             else:
                 q.append((loc2, now))
+                
         if 0<=loc3<=100000 and loc3 not in lst:
             lst.add(loc3)
             if loc3==k:
