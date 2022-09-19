f4714ebb8ef0bcaf9ab12b6490e2192c264108bf
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2022-09-19 16:54:34 +0900

    Create 11399_ATM_greedy.py

---
+++ BOJ/11399_ATM_greedy.py
@@ -0,0 +1,29 @@
+# code1
+import sys
+r = sys.stdin.readline
+
+n = int(r())
+lst = list(map(int, r().split(" ")))
+lst.sort()
+
+x = 0
+for i in range(len(lst)):
+    now = 0
+    for j in range(i+1):
+        now += lst[j]
+    x += now
+
+print(x)
+
+# code2 - 시간 단축
+import sys
+r = sys.stdin.readline
+
+n = int(r())
+lst = list(map(int, r().split(" ")))
+lst.sort()
+
+ans = sum(lst) * len(lst)
+for i in range(len(lst)-1, 0, -1):
+    ans -= lst[i] * i
+print(ans)
