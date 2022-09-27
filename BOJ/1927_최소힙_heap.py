c3f420ca57b314639e84e965e6929626a6e09351
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2022-09-27 22:28:29 +0900

    Create 1927_최소힙_heap.py

---
+++ BOJ/1927_최소힙_heap.py
@@ -0,0 +1,16 @@
+import sys
+from heapq import heappop, heappush
+r = sys.stdin.readline
+
+n = int(r())
+heap = []
+for _ in range(n):
+    x = int(r())
+    if x == 0:
+        if len(heap) == 0:
+            print("0")
+        else:
+            now = heappop(heap)
+            print(now)
+    else:
+        heappush(heap, x)
