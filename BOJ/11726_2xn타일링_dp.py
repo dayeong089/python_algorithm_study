732675e3d907fa0bec6eba2edce3c882c8eed4ef
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2022-10-02 16:44:06 +0900

    Create 11726_2xn타일링_dp.py

---
+++ BOJ/11726_2xn타일링_dp.py
@@ -0,0 +1,13 @@
+import sys
+r = sys.stdin.readline
+n = int(r())
+
+if n==1: print(1)
+elif n==2: print(2)
+elif n>=3:
+    lst = [0]*(n+1)
+    lst[1] = 1
+    lst[2] = 2
+    for i in range(3, n+1):
+        lst[i] = lst[i-1]+lst[i-2]
+    print(lst[n]%10007)
