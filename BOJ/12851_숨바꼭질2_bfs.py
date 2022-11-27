eca189a43e909cb9e9e4b9ade5d4f3a3251589fd
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2022-11-27 21:36:37 +0900

    Update 12851_숨바꼭질2_bfs.py

--- BOJ/12851_숨바꼭질2_bfs.py
+++ BOJ/12851_숨바꼭질2_bfs.py
@@ -26,7 +26,7 @@ while q:
         if cnt == 0:
             min_time = time # 최소 시간을 구해줌 - min_time
         cnt += 1 # 최소 시간으로 갈 수 있는 방법 수 - cnt
-    if cnt!=0 and time > min_time: # time이 최소 시간을 넘기면 
+    if cnt!=0 and time > min_time: # time이 최소 시간을 넘기면 stop
         print(min_time)
         print(cnt)
         sys.exit()
