ba031dc369601a1e28a4c637fc4fea48c3a9ed90
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2023-09-12 23:11:44 +0900

    [Silver III] Title: 통계학, Time: 428 ms, Memory: 52640 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,28 @@
+import sys
+r = sys.stdin.readline
+
+n = int(r())
+lst = []
+cnt = [0]*8001
+for _ in range(n):
+    x = int(r())
+    lst.append(x)
+    cnt[x+4000] += 1
+lst.sort()
+
+average = round(sum(lst)/n)
+print(average)
+
+mid = n//2
+print(lst[mid])
+
+freq_value = max(cnt)
+freq_num = cnt.count(freq_value)
+if freq_num == 1:
+    print(cnt.index(freq_value)-4000)
+else:
+    first_idx = cnt.index(freq_value)
+    cnt[first_idx] -= 1
+    print(cnt.index(freq_value)-4000)
+
+print(max(lst)-min(lst))
