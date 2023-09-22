bf4c97e559cd1dc55ad59585bbb7b7ac8554afc5
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2023-09-22 22:44:27 +0900

    Update iSharp.py

--- 백준/Silver/3568. iSharp/iSharp.py
+++ 백준/Silver/3568. iSharp/iSharp.py
@@ -1,27 +1,12 @@
 import sys
-r = sys.stdin.readline
+r = lambda:sys.stdin.readline().rstrip()
 
-sent = r().rstrip().split(" ")
-lst1 = [[] for _ in range(len(sent))]
-lst2 = [[] for _ in range(len(sent))]
-
+sent = r().split(" ")
 for i in range(1, len(sent)):
-    for j in sent[i]:
-        if j==',' or j==';' or j=='[':
-            pass
-        elif j==']':
-            lst1[i].append('[]')
-        elif j=='&' or j=='*':
-            lst1[i].append(j)
-        else:
-            lst2[i].append(j)
-
-for i in range(1, len(lst1)):
-    result = sent[0]
-    for j in range(len(lst1[i])-1, -1, -1):
-        result += lst1[i][j]
-    result += " "
-    for j in range(len(lst2[i])):
-        result += lst2[i][j]
-    result += ";"
-    print(result)
+    idx = 0
+    sent[i] = sent[i].replace(";",",")
+    for j in range(len(sent[i])):
+        if sent[i][j] in ["*", "&", "[", ","]:
+            idx = j
+            break
+    print(sent[0]+sent[i][j:][::-1].replace("][", "[]").replace(",", ""), sent[i][:j]+";")
