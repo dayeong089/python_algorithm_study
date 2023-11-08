61919fad236cc197d43456964c3e09c52dc3e70e
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2023-11-08 23:22:51 +0900

    [level 1] Title: 시저 암호, Time: 1.98 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -6,10 +6,10 @@ def solution(s, n):
         else:
             now_num = ord(i)
             next_num = now_num + n
-            if now_num >= ord('A') and now_num <= ord('Z'):
+            if i.isupper():
                 if next_num > ord('Z'):
                     next_num = next_num - ord('Z') + ord('A') - 1
-            elif now_num >= ord('a') and now_num <= ord('z'):
+            elif i.islower():
                 if next_num > ord('z'):
                     next_num = next_num - ord('z') + ord('a') - 1
             answer += chr(next_num)
