93ab1b4c31aae8280daac0910c36556d7a6f05c1
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2024-01-24 17:15:56 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -3,20 +3,13 @@ using namespace std;
 
 int main()
 {
-    int now_time;
-    int now_minute;
-    int after_time;
-    int after_minute;
-    int cook_time;
+    int now_time, now_minute, after_time, after_minute, cook_time;
 
-    cin >> now_time;
-    cin >> now_minute;
-    cin >> cook_time;
+    cin >> now_time >> now_minute >> cook_time;
 
     after_minute = now_minute + cook_time;
-    after_time = now_time + (after_minute/60);
+    after_time = (now_time + (after_minute/60)) % 24;
     after_minute = after_minute % 60;
-    if (after_time >= 24) { after_time %= 24; }
 
     cout << after_time << " " << after_minute << endl;
 
